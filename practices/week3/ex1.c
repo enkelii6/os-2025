2fd2853d652cecd82ecf588f52adf47cb4847bd9
enkeli <kukarsevgleb@gmail.com>
2025-01-30 18:42:50 +0500

    changed week3.ex_2.c

--- practices/week3/ex1.c
+++ practices/week3/ex1.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
 #include <time.h>
 
-void fun()
-{
-    printf("fun() starts \n");
-    printf("Press enter to stop fun \n");
-    while (1)
-    {
+void fun(void) {
+    printf("fun() starts\n");
+    printf("Press enter to stop fun\n");
+    
+    while (1) {
         if (getchar())
             break;
     }
-    printf("fun () ends \n");
+    
+    printf("fun() ends\n");
 }
 
-int main ()
-{
-    clock_t t;
-    t = clock();
+int main(void) {
+    clock_t start_time = clock();
+    
     fun();
-    t = clock() - t;
-    double time_taken = ((double)t)/CLOCKS_PER_SEC;
-    printf("fun() took %f seconds to execute \n", time_taken);
+    
+    clock_t end_time = clock();
+    double time_taken = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+    
+    printf("fun() took %.2f seconds to execute\n", time_taken);
+    
     return 0;
 }
